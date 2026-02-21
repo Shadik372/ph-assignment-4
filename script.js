f111cf08f2e9b3f9f399a43df93a1f4f4cf28630
Farhan Shadik <rageshadik96@gmail.com>
2026-02-22 05:40:30 +0600

    Added Delete button functionality in Index HTML

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-console.log('Connected');
-document.getElementById("interview-filter").addEventListener("click", function(){
+console.log("Connected");
+document
+  .getElementById("interview-filter")
+  .addEventListener("click", function () {
     window.location.href = "interview.html";
-});
-document.getElementById("rejected-filter").addEventListener("click", function(){
+  });
+document
+  .getElementById("rejected-filter")
+  .addEventListener("click", function () {
     window.location.href = "rejected.html";
+  });
+document.getElementById("all-filter").addEventListener("click", function () {
+  window.location.href = "index.html";
 });
-document.getElementById("all-filter").addEventListener("click", function(){
-    window.location.href = "index.html";
+
+// Showing Job Count and Update
+function updateCount() {
+  const cards = document.getElementsByClassName("job-card");
+  const count = cards.length;
+
+  document.getElementById("job-count").innerText = count;
+  document.getElementById("availableJobCount").innerText = count + " Jobs";
+}
+
+updateCount();
+
+//Delete Button Functionality
+const deleteButtons = document.querySelectorAll(".delete-button");
+
+deleteButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    const card = button.closest(".job-card");
+    card.remove();
+    updateCount();
+  });
 });
